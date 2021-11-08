465f717c44e7325ab3d07f347354a5852e2fc6c2
Bukola <bukolaebikefe@yahoo.com>
2021-11-08 17:50:33 +0100

    installed graphQl and ApolloClient

--- github_profilePage/src/App.jsx
+++ github_profilePage/src/App.jsx
@@ -2,13 +2,21 @@ import react from "react";
 import React from "react";
 import "./App.css";
 // import Body from "./body";
-import SignIn from "./SignIn"
+import SignIn from "./SignIn";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 
 function App() {
   return (
     <div className="App">
       {/* <Body /> */}
-      <SignIn/>
+      <SignIn />
     </div>
   );
 }
