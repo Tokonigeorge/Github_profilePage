acf177b6027346a01cc63446dcb21a305de68006
Bukola <bukolaebikefe@yahoo.com>
2021-11-11 16:38:18 +0100

    worked on review Pr activity UI

--- github_profilePage/src/sections/activity.jsx
+++ github_profilePage/src/sections/activity.jsx
@@ -24,10 +24,16 @@ const Activity = () => {
         isPull={true}
       />
       <Activities
-        Icon={PullIcon}
+        Icon={RepoIcon}
         contribution_no={2}
         repo_no={5}
         isCreated={true}
+      />
+      <Activities
+        Icon={ReviewIcon}
+        contribution_no={2}
+        repo_no={5}
+        isReview={true}
       />
       <button
         type="button"
@@ -53,6 +59,7 @@ const Activities = ({
   isCommit,
   isPull,
   isCreated,
+  isReview,
 }) => {
   const [open, setOpen] = useState(true);
   return (
@@ -116,6 +123,14 @@ const Activities = ({
             />
           </>
         )}
+        {open && isReview && (
+          <>
+            <ReviewActivity url="#" name="something" pullrequest_no={2} />
+            <ReviewActivity url="#" name="something" pullrequest_no={2} />
+            <ReviewActivity url="#" name="something" pullrequest_no={2} />
+            <ReviewActivity url="#" name="something" pullrequest_no={2} />
+          </>
+        )}
       </div>
     </>
   );
@@ -171,10 +186,25 @@ const CreatedRepoActivity = ({
         </a>
       </span>
       <span className="flex items-center">
-        <span className="w-2 h-2 rounded-full bg-purple-300"></span>
+        <span className="w-2.5 h-2.5 rounded-full bg-purple-600 ring-1 ring-gray-600 mr-1"></span>
         <p>{language_name}</p>
       </span>
       <span>{createdAt}</span>
+    </div>
+  );
+};
+
+const ReviewActivity = ({ url, name, pullrequest_no }) => {
+  return (
+    <div className="flex items-center justify-between text-xs pt-1 mt-px pl-5">
+      <a href={url} className="text-blue-400 hover:underline text-sm">
+        {name}
+      </a>
+      <p>
+        {pullrequest_no > 1
+          ? `${pullrequest_no} pull requests`
+          : `${pullrequest_no} pull request`}
+      </p>
     </div>
   );
 };
@@ -228,6 +258,25 @@ const ToggleIcon = () => {
       aria-hidden="true"
     >
       <path d="M10.896 2H8.75V.75a.75.75 0 00-1.5 0V2H5.104a.25.25 0 00-.177.427l2.896 2.896a.25.25 0 00.354 0l2.896-2.896A.25.25 0 0010.896 2zM8.75 15.25a.75.75 0 01-1.5 0V14H5.104a.25.25 0 01-.177-.427l2.896-2.896a.25.25 0 01.354 0l2.896 2.896a.25.25 0 01-.177.427H8.75v1.25zm-6.5-6.5a.75.75 0 000-1.5h-.5a.75.75 0 000 1.5h.5zM6 8a.75.75 0 01-.75.75h-.5a.75.75 0 010-1.5h.5A.75.75 0 016 8zm2.25.75a.75.75 0 000-1.5h-.5a.75.75 0 000 1.5h.5zM12 8a.75.75 0 01-.75.75h-.5a.75.75 0 010-1.5h.5A.75.75 0 0112 8zm2.25.75a.75.75 0 000-1.5h-.5a.75.75 0 000 1.5h.5z"></path>
+    </svg>
+  );
+};
+
+const ReviewIcon = () => {
+  return (
+    <svg
+      aria-hidden="true"
+      height="16"
+      viewBox="0 0 16 16"
+      version="1.1"
+      width="16"
+      data-view-component="true"
+      className="fill-current"
+    >
+      <path
+        fillRule="evenodd"
+        d="M1.679 7.932c.412-.621 1.242-1.75 2.366-2.717C5.175 4.242 6.527 3.5 8 3.5c1.473 0 2.824.742 3.955 1.715 1.124.967 1.954 2.096 2.366 2.717a.119.119 0 010 .136c-.412.621-1.242 1.75-2.366 2.717C10.825 11.758 9.473 12.5 8 12.5c-1.473 0-2.824-.742-3.955-1.715C2.92 9.818 2.09 8.69 1.679 8.068a.119.119 0 010-.136zM8 2c-1.981 0-3.67.992-4.933 2.078C1.797 5.169.88 6.423.43 7.1a1.619 1.619 0 000 1.798c.45.678 1.367 1.932 2.637 3.024C4.329 13.008 6.019 14 8 14c1.981 0 3.67-.992 4.933-2.078 1.27-1.091 2.187-2.345 2.637-3.023a1.619 1.619 0 000-1.798c-.45-.678-1.367-1.932-2.637-3.023C11.671 2.992 9.981 2 8 2zm0 8a2 2 0 100-4 2 2 0 000 4z"
+      ></path>
     </svg>
   );
 };
